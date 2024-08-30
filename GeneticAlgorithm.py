065873ca371a12a28c2bc72229b45a4246766044
Daniel Cruz <daniel.jack.dmc@gmail.com>
2024-08-29 22:11:51 -0300

    fix: Crossover refector

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -5,30 +5,43 @@ class GeneticAlgorithm:
     def __init__(
         self, population: list[dict[str, int]], selecteds: int = 2, mutation_rate: float = 0.1
     ):
-        self.results = []
-        self.population = population
+        self.results: list = []
+        self.population: list = population
         self.mutation_rate: float = mutation_rate
         self.selecteds: int = selecteds
 
-    def crossover(self, total_iterations: int, *parent: list):
-        new_population = []
+    def crossover(self, total_iterations: int, parents: list):
+        new_population: list = []
 
         # Copiando a população original para a nova população
         new_population.extend(self.population)
 
-        while len(new_population) < total_iterations:
-            child = []
-            for i in range(3):
-                all_reds = [par_[i]['redDuration'] for par_ in parent]
-                all_greens = [par_[i]['greenDuration'] for par_ in parent]
-                all_cycles = [par_[i]['cycleStartTime'] for par_ in parent]
-                child.append({
-                    'redDuration': random.choice(all_reds),
-                    'greenDuration': random.choice(all_greens),
-                    'cycleStartTime': random.choice(all_cycles),
-                })
+        all_reds: list = []
+        all_greens: list = []
+        all_cycles: list = []
 
-            new_population.append(child)
+        for parent in parents:
+            all_reds.append([crossing['redDuration'] for crossing in parent])
+            all_greens.append([crossing['greenDuration'] for crossing in parent])
+            all_cycles.append([crossing['cycleStartTime'] for crossing in parent])
+
+        while len(new_population) < (total_iterations - len(parents)):
+            reds_selecteds = []
+            greens_selecteds = []
+            cycles_selecteds = []
+
+            for i in range(len(parents) - 1):
+                reds_selecteds.append(all_reds[random.range(2)][i])
+                greens_selecteds.append(all_greens[random.range(2)][i])
+                cycles_selecteds.append(all_cycles[random.range(2)][i])
+
+            new_population.append({
+                'redDuration': random.choice(reds_selecteds),
+                'greenDuration': random.choice(greens_selecteds),
+                'cycleStartTime': random.choice(cycles_selecteds),
+            })
+
+        self.population = new_population
         return new_population
 
     def mutate(self, individual):
@@ -40,3 +53,8 @@ class GeneticAlgorithm:
             if random.random() < self.mutation_rate:
                 semaphore['cycleStartTime'] = random.randint(1, 120)
         return individual
+
+    def select(self, all_results: list):
+        # ordena os resultados pelo tempo médio de viagem e no final pega os N melhores
+        all_results.sort(key=lambda x: x['tripAvg'])
+        return all_results[: self.selecteds]
